779df18455ea8ad2f7f9d2c59b3192f7835bfd6f
Alba <albamrus@gmail.com>
2023-02-03 10:53:25 +0100

    view test added (fail)

--- tests/Feature/CRUDStudentTest.php
+++ tests/Feature/CRUDStudentTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Models\Student;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Factory;
 
 class CRUDStudentTest extends TestCase
 {
@@ -47,5 +48,15 @@ class CRUDStudentTest extends TestCase
 
         $this->assertCount(1,Student::all()); 
 
+    }
+public function testListStudentAppearsInStudentView()
+    {
+        $this->withoutExceptionHandling();
+        $students = Student::factory(2)->create();
+        $student = $students->first();
+        $response = $this->get(route('student.index'));
+        $response->assertSee($student->name)
+                 ->assertStatus(200)
+                 ->assertViewIs('student.index');
     }
 }
