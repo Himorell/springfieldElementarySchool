80e0da6d4de90b884ad538513f6344454cb413b9
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-02-14 22:22:58 +0100

    index.js modified

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,17 @@
-const btn_foto = document.querySelector('btn-photo');
+let button_photo = document.querySelector('#btn-photo');
+let image = document.querySelector('#user-photo');
 
 let widGet_cloudinary = cloudinary.createUploadWidget({
     cloudName : '',
     uploadPreset: '',
-}(err, result)=>{
-    if(!err && result && result.event === 'sucess');{
+
+}, (err, result)=>{
+    if(!err && result && result.event === 'success'){
         console.log('Image uploaded successfully', result.info);
+        image.src = (result.info.secure_url);
     }
 });
 
-btn_foto.addEventListener('click', ()=>{})
+button_photo.addEventListener('click', ()=>{
+    widGet_cloudinary.open();
+}, false);
