bdf61ad0cdc3b2f6c4863ce16fa10f67600891ec
CarmenCruces <karmelacfer@gmail.com>
2023-02-17 15:22:38 +0100

    ubicationLogo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/png" sizes="512x512" href="{{ asset('/images/favicon.png') }}">
+    <link rel="shortcut icon" type="image/png" sizes="512x512" href="{{ asset('/css/img/favicon.png') }}">
     <title>{{ ('Springfield Elementary School') }}</title>
 
 
@@ -24,11 +24,11 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm opacity-75">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm opacity-75" >
+            <div class="container" >
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <a class="navbar-brand" href="#">
-                        <img src="css/imgwelcome/logo.png" alt="Bootstrap" width="" height="100">
+                        <img src="css/imgwelcome/logo.png" alt="Logo Springfield Elementary School" width="" height="200">
                     </a>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
